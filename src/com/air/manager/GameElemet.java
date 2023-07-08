a0e11b35dc2a6d537292eb9b2dbbb1c3d7100ba3
christyeeee <106256035+christyeeee@users.noreply.github.com>
2023-07-08 17:03:21 +0800

    Update GameElemet.java

--- src/com/air/manager/GameElemet.java
+++ src/com/air/manager/GameElemet.java
@@ -2,11 +2,15 @@ package com.air.manager;
 
 public enum GameElemet {
 	/**
+	 * @说明 游戏元素
 	 * PLAY 玩家
 	 * MAPS 地图
 	 * ENEMY 敌人
-	 * BOSS　大BOSS
+	 * BOSS  boss
+	 * PLAYFIRE 子弹
+	 * DIE 死亡
+	 * PROP 道具
+	 * EXPLODED 爆炸
 	 */
-	MAPS,PLAY,ENEMY,BOSS,PLAYFILE,DIE,NEXT;  //枚举顺序是声明顺序
-//	我们定义的枚举类型，在编译的时候，虚拟机会自动帮助生存class文件，并会加载许多代码和方法
+	MAPS,PLAY,ENEMY,BOSS,PLAYFILE,DIE,PROP,EXPLODED;  //枚举类型的顺序是 声明的顺序
 }
