87017759bd88c7182c14a386118d501919d1633f
tobesirius <2932916203@qq.com>
2023-07-09 14:25:27 +0800

    控制器

---
+++ src/com/air/controller/GameListener.java
@@ -0,0 +1,62 @@
+package com.air.controller;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.air.element.ElementObj;
+import com.air.manager.ElementManager;
+import com.air.manager.GameElement;
+
+/**
+ * @说明 监听类 用于监听用户的操作KeyListener
+ * @author 墨洋
+ *
+ */
+public class GameListener implements KeyListener{
+
+	private ElementManager em=ElementManager.getManager();
+	
+	/*通过一个集合来记录所有按下的键，如果重复触发，就直接结束*/
+	private Set<Integer> set=new HashSet<Integer>();
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+
+	/**
+	 * 按下：左37 上38 右39 下40 按tab没有反应
+	 * 实现主角的移动
+	 */
+	@Override
+	public void keyPressed(KeyEvent e) {
+//		拿到玩家集合
+//		System.out.println("按下"+e.getKeyCode());
+		int key=e.getKeyCode();
+		if (set.contains(key)) {//板顶集合中是否已经存在，包含这个对象
+//			如果包含直接结束方法
+			return;
+		}
+		set.add(key);
+		List<ElementObj> play = em.getElementsByKey(GameElement.PLAY);
+		for (ElementObj obj : play) {
+			obj.keyClick(true,e.getKeyCode());
+		}
+	}
+
+	/**松开*/
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (!set.contains(e.getKeyCode())) {
+			return;
+		}
+		set.remove(e.getKeyCode());//移除数据
+		List<ElementObj> play = em.getElementsByKey(GameElement.PLAY);
+		for (ElementObj obj : play) {
+			obj.keyClick(false,e.getKeyCode());
+		}
+	}
+
+}
---
+++ src/com/air/controller/GameThread.java
@@ -0,0 +1,135 @@
+package com.air.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+
+import com.air.element.ElementObj;
+import com.air.element.Enemy1;
+import com.air.element.Play;
+import com.air.manager.ElementManager;
+import com.air.manager.GameElement;
+import com.air.manager.GameLoad;
+
+/**
+ * @说明 游戏的主线程，用于控制游戏加载，游戏关卡，游戏运行时自动化
+ * 		游戏判定
+ * @author 墨洋
+ * @继承 使用继承的方式实现多线程（一般建议使用接口实现）
+ */
+public class GameThread extends Thread{
+
+	private ElementManager em;
+	
+	public GameThread() {
+		em=ElementManager.getManager();
+	}
+	
+	@Override
+	public void run() {//游戏的run方法 主线程
+		while (true) {
+//		游戏开始前 读进度条，加载游戏资源（场景资源）
+			gameLoad();
+//		游戏进行时 游戏过程中
+			gameRun();
+//		游戏场景结束 游戏资源回收（场景资源）
+			gameOver();
+			
+			try {
+				sleep(50);
+			} catch (InterruptedException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * 游戏的加载
+	 */
+	private void gameLoad() {
+		GameLoad.loadImg();//加载图片
+//		GameLoad.MapLoad(2);//可变为变量，每一关重新加载//加载地图
+//		加载主角
+		GameLoad.loadPlay();//可带参数，单机还是2人
+		
+//		加载敌人NPC
+//		GameLoad.loadEnemy();
+	}
+	/**
+	 * 游戏进行时
+	 * 1.自动化玩家的移动 碰撞 死亡
+	 * 2.新元素的增加（NPC死亡后出现道具）
+	 * 3.游戏暂停。。。
+	 * 先实现主角的移动
+	 */
+	private int gameTime=0;
+	private void gameRun() {
+		while (true) {//true可以变为变量，用于控制关卡结束等
+			Map<GameElement, List<ElementObj>> all = em.getGameElements();
+			List<ElementObj> enemys = em.getElementsByKey(GameElement.ENEMY);
+			List<ElementObj> files = em.getElementsByKey(GameElement.PLAYFILE);
+			List<ElementObj> maps = em.getElementsByKey(GameElement.MAPS);
+			moveAndUpdate(all, gameTime);//	游戏元素自动化方法
+			
+			ElementPK(enemys,files);
+			ElementPK(files,maps);
+			
+			gameTime++;//唯一的时间控制
+			try {
+				sleep(10);
+			} catch (InterruptedException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void ElementPK(List<ElementObj> listA,List<ElementObj> listB) {
+		
+		for (int i = 0; i < listA.size(); i++) {
+			ElementObj enemy=listA.get(i);
+			for (int j = 0; j < listB.size(); j++) {
+				ElementObj file=listB.get(j);
+				if (enemy.pk(file)) {
+//					攻击boss：将setLive(false)变为一个受攻击方法，还可以传入另外一个对象的攻击力
+//					当受攻击方法里执行时，如果血量减为0，再将生存设置为false
+					System.out.println();
+					enemy.setLive(false);
+					file.setLive(false);
+					break;
+				}
+			}
+		}
+	}
+
+//	游戏元素自动化方法
+	public void moveAndUpdate(Map<GameElement, List<ElementObj>> all,int gameTime) {
+//		GameElement.values();//隐藏方法 返回值是一个数组
+		for (GameElement ge : GameElement.values()) {
+			List<ElementObj> list = all.get(ge);
+//			for (int i = 0; i < list.size(); i++) {
+			for (int i = list.size()-1; i >= 0; i--) {
+				ElementObj obj=list.get(i);//读取为基类
+				if (!obj.isLive()) {//如果死亡
+//					list.remove(i--);
+//					启动一个死亡方法（死亡动画，掉装备）
+					obj.die();
+					list.remove(i);
+					continue;
+				}
+				obj.model(gameTime);
+			}
+		}
+	}
+	
+	
+	/**
+	 * 游戏切换关卡
+	 */
+	private void gameOver() {
+		
+	}
+	
+}
