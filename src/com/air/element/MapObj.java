066cc007fe8a2378cf37863e6e2f72d26b58f904
christyeeee <106256035+christyeeee@users.noreply.github.com>
2023-07-09 16:48:30 +0800

    GameLoad

--- src/com/air/element/MapObj.java
+++ src/com/air/element/MapObj.java
@@ -11,50 +11,52 @@ import com.air.manager.GameLoad;
 import com.air.show.GameJFrame;
 
 public class MapObj extends ElementObj{
-	public static Image bg = Toolkit.getDefaultToolkit().getImage("image/background/1.png");
-	int height = 630;//图片高度
-	int firsty = 0,secondy = -700;//firsty第一张图的y坐标；secondy第二张图的y坐标
-	protected int number=1;//关卡，默认1
-	public MapObj() {}
-	public MapObj(int x,int y,int w,int h,ImageIcon icon) {
-		super(x,y,w,h,icon);
-	}
-
-	public ElementObj creatElement(String str) {//str：x,y,icon
-		String[] split = str.split(",");
-		this.setX(Integer.parseInt(split[0]));
-		this.setY(Integer.parseInt(split[1]));
-		ImageIcon icon=GameLoad.imgMap.get(split[2]);
-		this.setW(icon.getIconWidth());
-		this.setH(icon.getIconHeight());
-		this.setIcon(icon);
-//		随关卡变换背景
-		return this;
+	
+	// private ImageIcon img=new ImageIcon("img/Background/1.png");
+	 private int moveY=0; //从图片顶端向下滚动
+	 private int moveYY=800;//从图片底端向上滚动，大小等于图片的长度
+	 private String bg="bg1";
+	 
+	 public MapObj() {}
+	 public MapObj(int x,int y,int w,int h,ImageIcon icon) {
+		 super(x,y,w,h,icon);
+	 }
+	 
+	 @Override
+		public ElementObj createElement(String str) {
+			String[] split = str.split(",");
+			this.setX(Integer.parseInt(split[0]));
+			this.setY(Integer.parseInt(split[1]));
+			ImageIcon icon2 = GameLoad.imgMap.get(split[2]);
+			this.setW(icon2.getIconWidth());
+			this.setH(icon2.getIconHeight());
+			this.setIcon(icon2);
+			return this;
+		}
+	@Override
+	public void showElement(Graphics g) {	
+		// TODO 自动生成的方法存根
+		 g.drawImage(this.getIcon().getImage(), 
+				 this.getX(), this.getY(),  
+				 this.getW(), this.getH(), null);
+		 g.drawImage(this.getIcon().getImage(), 
+				 this.getX(), this.getY(),  
+				 this.getW(), this.getH(), null);
 	}
 
 	@Override
-	public void showElement(Graphics g) {
-		g.drawImage(this.bg, 
-				0, firsty, 
-				775, height, null);
-		g.drawImage(this.bg, 
-				0, secondy, 
-				775, height, null);
+	protected void move(int gameTime) {
+		// TODO 自动生成的方法存根
+		super.move(gameTime);
+		this.setY(this.getY()+gameTime);
+		
+//		moveY=(moveY>=800)?0:moveY;
+//		moveY+=gameTime;
+//		moveYY=(moveYY<=0)?800:moveYY;
+//		 moveYY-=gameTime; 
 	}
 	
-	public void move() {
-		if (secondy>=620) {
-			secondy=firsty-height;
-		}
-		else if (firsty>=620) {
-			firsty=secondy-height;
-		}
-		secondy+=3;
-		firsty+=3;
+	protected void updateImage() {
+		this.setIcon(GameLoad.imgMap.get(bg));
 	}
-	
-//	public void updateImage() {
-//		this.setIcon(GameLoad.imgMap.get(number));
-//	}
-
 }
