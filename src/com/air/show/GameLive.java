e38250b22edf1153cdeeb1af6df5de9f5051eeab
yierear <104077071+yierear@users.noreply.github.com>
2023-07-10 12:43:37 +0800

    关卡

--- src/com/air/show/GameLive.java
+++ src/com/air/show/GameLive.java
@@ -18,28 +18,41 @@ public class GameLive extends JPanel implements Runnable{
 	}
 	
 	public void init() {
-		//血条的设置
-		Font font1 = new Font("Broadway", Font.ITALIC, 10);
 		this.setLayout(null);
+		//关卡的显示
+		JLabel jLabel = new JLabel("关卡:");
+		jLabel.setBounds(0, 10, 30, 15);
+		Font font = new Font("Broadway", Font.BOLD, 35);
+		String str = GameLevel.getLevel()+"";
+ 		JLabel jLabel0 = new JLabel(str);
+ 		jLabel0.setForeground(Color.ORANGE);
+ 		jLabel0.setFont(font);
+ 		jLabel0.setBounds(50, 0, 100, 40);
+		this.add(jLabel);
+		this.add(jLabel0);
+		
+		//血条的设置
+		Font font1 = new Font("Broadway", Font.ITALIC, 15);
 		JLabel jLabel1 = new JLabel("hp:");
-		jLabel1.setBounds(0, 10, 20, 15);
+		jLabel1.setBounds(0, 40, 20, 15);
 		String str1 = Play.getHp()+"";
 		JLabel jLabel2 = new JLabel(str1);
 		jLabel2.setFont(font1);
 		jLabel2.setForeground(Color.RED);
-		jLabel2.setBounds(135, 10, 25, 15);
+		jLabel2.setBounds(125, 40, 35, 15);
 		this.add(jLabel1);
 		this.add(jLabel2);
 		
 		//得分的设置
 		Font font2 = new Font("Broadway", Font.BOLD, 35);
 		JLabel jLabel3 = new JLabel("得分:");
-		jLabel3.setBounds(0, 55, 30, 15);
+		jLabel3.setBounds(0, 75, 30, 15);
 		String str2 = GameThread.getScore()+"";
+		System.out.println(str2);
 		JLabel jLabel4 = new JLabel(str2);
 		jLabel4.setFont(font2);
 		jLabel4.setForeground(Color.BLUE);
-		jLabel4.setBounds(50, 20, 100, 80);
+		jLabel4.setBounds(50, 60, 100, 40);
 		this.add(jLabel3);
 		this.add(jLabel4);
 	}
@@ -49,8 +62,8 @@ public class GameLive extends JPanel implements Runnable{
 		// TODO 自动生成的方法存根
 		super.paint(g);
 		g.setColor(Color.red);
-		g.drawRect(30, 10, 100, 15);
-		g.fillRect(30, 10, Play.getHp(), 15);
+		g.drawRect(25, 40, 100, 15);
+		g.fillRect(25, 40, Play.getHp(), 15);
 	}
 	
 	@Override
