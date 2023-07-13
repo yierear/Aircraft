892c53ac94524821ab8009f74a246f9a1b648a51
yierear <104077071+yierear@users.noreply.github.com>
2023-07-13 18:22:26 +0800

    分类血量

--- src/com/air/element/Enemy.java
+++ src/com/air/element/Enemy.java
@@ -31,7 +31,6 @@ public class Enemy extends ElementObj{
 		this.setY(Integer.parseInt(split[1]));
 		this.setW(Integer.parseInt(split[2]));	
 		this.setH(Integer.parseInt(split[3]));
-		this.setHp(3);
 		enemyType=split[4];
 		ImageIcon icon = GameLoad.imgMap.get(enemyType);
 		this.setIcon(icon);
@@ -93,6 +92,14 @@ public class Enemy extends ElementObj{
 		int y=this.getY()+this.getIcon().getIconHeight();
 		x+=(this.getIcon().getIconWidth())/2;
 		return "x:"+x+",y:"+y+",f:"+this.fx+",type:"+enemyType;
+	}
+	
+	@Override
+	public void setHp(int hp) {
+		// TODO 自动生成的方法存根
+		switch(enemyType) {
+			
+		}
 	}
 	
 }
